9ccd6dc472ea81df6f7354a91cd4fa11e874e9c7
h1t3ndr4 <hitumali10@gmail.com>
2022-01-21 09:45:58 +0530

    landing page

---
+++ components/header.js
@@ -0,0 +1,98 @@
+function header() {
+  return `
+  
+  <div class="hamburger">
+    <span class="fas fa-bars"></span>
+  </div>
+  <div class="logo">
+    <a href=""
+      ><img
+        height="140"
+        width="280"
+        src="https://www.logo.wine/a/logo/Zara_(retailer)/Zara_(retailer)-Logo.wine.svg"
+        alt=""
+    /></a>
+
+
+    
+    <span class="rightoptions">
+    <a href="">
+    <p id="search-input">SEARCH</p></a>
+      <a href="">
+        <p>LOG IN</p>
+      </a>
+      <a href="">
+        <p>HELP</p>
+      </a>
+      <a href="">
+        <p>CART</p>
+      </a>
+    </span>
+  </div>
+
+
+
+
+
+  <nav class="sidebar">
+    <ul>
+      <li>
+        <a href="#" class="womensec">women</a>
+        <ul class="womensec1">
+          <li><a href="#">STARTING FROM 40%</a></li>
+          <li><a href="#">JACKETS | PUFFERS</a></li>
+          <li>
+            <a href="#">BLAZERS | COATS</a>
+            <ul>
+              <li><a href="#">DRESSES | JUMPSUITS</a></li>
+              <li><a href="#">SHIRTS</a></li>
+              <li><a href="#">TROUSERS</a></li>
+              <li><a href="#">JEANS</a></li>
+              <li><a href="#">T-SHIRTS</a></li>
+              <li><a href="#">TOPS | BODYSUITS</a></li>
+              <li><a href="#">SKIRTS | SHORTS</a></li>
+              <li><a href="#">KNITWEAR</a></li>
+              <li><a href="#">SHOES</a></li>
+              <li><a href="#">BAGS</a></li>
+              <li><a href="#">ACCESSORIES</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+
+
+
+      <li>
+        <a href="#" class="mensec">man</a>
+        <ul class="mensec1">
+          <li><a href="#">STARTING FROM -60%</a></li>
+          <li><a href="#">STARTING FROM -40%</a></li>
+          <li>
+            <a href="#">JACKETS | COATS</a>
+            <ul>
+              <li><a href="#">OVERSHIRTS</a></li>
+              <li><a href="#">SHIRTS</a></li>
+              <li><a href="#">T-SHIRTS | SWEATSHIRTS</a></li>
+              <li><a href="#">TROUSERS | JEANS</a></li>
+              <li><a href="#">SHOES</a></li>
+              <li><a href="#">BAGS | BACKPACKS</a></li>
+              <li><a href="#">ACCESSORIES</a></li>
+              <li><a href="#">ZARA ATHLETICS</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+
+
+
+
+      <li><a href="#">Kids</a></li>
+      <li><a href="#">SHOES & BAGS</a></li>
+      <li><a href="#">JOIN LIFE</a></li>
+      <br />
+      <li><a href="#">+ info</a></li>
+    </ul>
+  </nav>`;
+}
+
+export { header };
