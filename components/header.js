1f149644e20bfb0e28c0dccdaafd89fa049b0aeb
h1t3ndr4 <hitumali10@gmail.com>
2022-01-22 16:32:03 +0530

    some changes in transfer

--- components/header.js
+++ components/header.js
@@ -40,7 +40,7 @@ function header() {
         <a href="#" class="womensec">women</a>
         <ul class="womensec1">
           <li><a href="#">STARTING FROM 40%</a></li>
-          <li><a href="women_jackets.html">JACKETS | PUFFERS</a></li>
+          <li id="womenJacks">JACKETS | PUFFERS</li>
           <li>
             <a href="#">BLAZERS | COATS</a>
             <ul>
@@ -54,7 +54,7 @@ function header() {
               <li><a href="#">KNITWEAR</a></li>
               <li><a href="#">SHOES</a></li>
               <li><a href="#">BAGS</a></li>
-              <li id="womenAcc">ACCESSORIES</a></li>
+              <li id="womenAcc">ACCESSORIES</li>
             </ul>
           </li>
         </ul>
@@ -71,13 +71,13 @@ function header() {
             <a href="#">JACKETS | COATS</a>
             <ul>
               <li><a href="#">OVERSHIRTS</a></li>
-              <li><a href="mens_shirt.html">SHIRTS</a></li>
+              <li id="menShirts">SHIRTS</li>
               <li><a href="#">T-SHIRTS | SWEATSHIRTS</a></li>
               <li><a href="#">TROUSERS | JEANS</a></li>
-              <li><a href="mens_shoes.html">SHOES</a></li>
+              <li id="menShoes">SHOES</li>
               <li><a href="#">BAGS | BACKPACKS</a></li>
               <li><a href="#">ACCESSORIES</a></li>
-              <li><a href="zaraAthleticz.html">ZARA ATHLETICS</a></li>
+              <li id="athleticz">ZARA ATHLETICS</li>
             </ul>
           </li>
         </ul>
